1dba6912d0baaec128168bce2eb2f2c89bd77b40
Naaziiirr14 <sheikh.naazir114@gmail.com>
2026-04-27 11:19:05 +0530

    Updated

--- frontend/src/components/api.jsx
+++ frontend/src/components/api.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NODE_ENV === "production"
+    ? "/_/backend/api"
+    : "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
 });
 
